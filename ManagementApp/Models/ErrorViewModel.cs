c0e919be0beb946e6ed5956483842b895d156c47
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-22 22:12:06 +0300

    First Commit. Project Create

---
+++ ManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
