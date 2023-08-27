8665b7436b30bc98d2ee799696f79a0b493047ce
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-27 18:40:39 +0300

    Setting up DBCONTEXT

---
+++ ManagementApp/Data/ManagementDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ManagementApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementApp.Data
+{
+    public class ManagementDbContext : DbContext
+    {
+        public ManagementDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- ManagementApp/Startup.cs
+++ ManagementApp/Startup.cs
@@ -1,5 +1,7 @@
+using ManagementApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,8 @@ namespace ManagementApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ManagementDbContext>(options =>
+            options.UseSqlServer(""));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
