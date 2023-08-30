285dedd7f4aa130b1312a367d026009831fbea54
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-30 20:21:37 +0300

    Complete adding a new employee

--- ManagementApp/Controllers/EmployeesController.cs
+++ ManagementApp/Controllers/EmployeesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ManagementApp.Data;
+using ManagementApp.Models.Domain;
+using ManagementApp.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +11,34 @@ namespace ManagementApp.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly ManagementDbContext managementDbContext;
+
+        public EmployeesController(ManagementDbContext managementDbContext)
+        {
+            this.managementDbContext = managementDbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployee addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
 
+            await managementDbContext.Employees.AddAsync(employee);
+            await managementDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
     }
 }
