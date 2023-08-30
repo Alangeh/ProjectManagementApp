00a4065a629c55552c40b57d8c8908ce8383c588
Lionel Ngobesing <ngobesinglionel@gmail.com>
2023-08-30 19:49:55 +0300

    Adding actions

---
+++ ManagementApp/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
